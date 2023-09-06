5ecb0968cfcc32d08408468be990a3e06d589caa
mauzec <49997318+Mauzec@users.noreply.github.com>
2023-09-06 15:22:25 +0300

    Initial Commit

---
+++ WorkersGenerator/WorkersGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkersGeneratorApp.swift
+//  WorkersGenerator
+//
+//  Created by Timur Murzakov on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkersGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
